8b37920374315a19392a2e36e14a127e0e3c1177
mahanhur <allowbasmh@gmail.com>
2023-05-11 17:51:41 +0900

    Initial commit

---
+++ src/test/java/com/kbstar/Clone0511ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kbstar;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@Slf4j
+@SpringBootTest
+class Clone0511ApplicationTests {
+
+
+    @Test
+    void contextLoads() {
+    }
+}
